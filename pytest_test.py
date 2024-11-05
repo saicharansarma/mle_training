9d11ef4f6cc76bc84d6325c2ae16916388dbe709
saicharan <saicharansarma.k@tigeranalytics.com>
2024-11-05 16:53:50 +0530

    Update pytest_test.py

--- pytest_test.py
+++ pytest_test.py
@@ -1,6 +1,7 @@
-def test_package_import():
-    try:
-        from mypackage import nonstandardcode
-    except ImportError as e:
-        assert False, f"Import failed: {e}"
-    assert True, "Package installed and import successful"
+# content of test_sample.py
+def func(x):
+    return x + 1
+
+
+def test_answer():
+    assert func(3) == 4
