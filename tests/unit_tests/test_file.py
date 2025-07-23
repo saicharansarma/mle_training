3bd45f15b8ac7d23e3aa6a0ca6fcad21837b2d47
saicharan <saicharansarma.k@tigeranalytics.com>
2025-07-23 16:20:36 +0530

    Update test_file.py

--- tests/unit_tests/test_file.py
+++ tests/unit_tests/test_file.py
@@ -11,7 +11,7 @@ def test_package_installation():
     #         "-m",
     #         "pip",
     #         "install",
-    #         "./dist/HousePricingPredictor-0.0.1-py3-none-any.whl",
+    #         "./dist/unknown-0.0.0-py3-none-any.whl",
     #     ],
     #     capture_output=True,
     #     text=True,
@@ -20,32 +20,33 @@ def test_package_installation():
     # assert result.returncode == 0, f"Installation failed: {result.stderr}"
 
     try:
-        import house_pricing_predictor
+        import src.house_pricing_predictor
     except ImportError as e:
         pytest.fail(f"Package import failed: {str(e)}")
 
     try:
-        from house_pricing_predictor import main
+        from src.house_pricing_predictor import main
     except ImportError as e:
         pytest.fail(f"Failed to import main module: {str(e)}")
 
     try:
-        from house_pricing_predictor import data_ingestion
+        from src.house_pricing_predictor import data_ingestion
     except ImportError as e:
         pytest.fail(f"Failed to import DataIngestion module: {str(e)}")
 
     try:
-        from house_pricing_predictor import model_training
+        from src.house_pricing_predictor import model_training
     except ImportError as e:
         pytest.fail(f"Failed to import ModelTraining module: {str(e)}")
 
     try:
-        from house_pricing_predictor import model_scoring
+        from src.house_pricing_predictor import model_scoring
     except ImportError as e:
         pytest.fail(f"Failed to import ModelScoring module: {str(e)}")
 
     try:
-        from house_pricing_predictor.data_ingestion import load_housing_data
+        from src.house_pricing_predictor.data_ingestion import load_housing_data
+
         assert callable(load_housing_data)
     except ImportError as e:
         pytest.fail(
@@ -53,7 +54,8 @@ def test_package_installation():
         )
 
     try:
-        from house_pricing_predictor.model_training import model_training
+        from src.house_pricing_predictor.model_training import model_training
+
         assert callable(model_training)
     except ImportError as e:
         pytest.fail(
@@ -61,7 +63,8 @@ def test_package_installation():
         )
 
     try:
-        from house_pricing_predictor.model_scoring import model_scoring
+        from src.house_pricing_predictor.model_scoring import model_scoring
+
         assert callable(model_scoring)
     except ImportError as e:
         pytest.fail(
