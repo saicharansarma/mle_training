c4fe389e187afda561212ae92577bcf1c2d00df4
saicharansarma <saicharansarma.k@tigeranalytics.com>
2025-07-24 11:39:57 +0000

    updates test file

--- tests/unit_tests/test_file.py
+++ tests/unit_tests/test_file.py
@@ -1,51 +1,44 @@
+import os
 import subprocess
 import sys
 
 import pytest
 
+# Add the src directory to sys.path
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../src"))
+)
+
 
 def test_package_installation():
-    # result = subprocess.run(
-    #     [
-    #         sys.executable,
-    #         "-m",
-    #         "pip",
-    #         "install",
-    #         "./dist/unknown-0.0.0-py3-none-any.whl",
-    #     ],
-    #     capture_output=True,
-    #     text=True,
-    # )
-
-    # assert result.returncode == 0, f"Installation failed: {result.stderr}"
-
+    """Test if the package can be imported and its modules are accessible."""
     try:
-        import src.house_pricing_predictor
+        import house_pricing_predictor
     except ImportError as e:
         pytest.fail(f"Package import failed: {str(e)}")
 
     try:
-        from src.house_pricing_predictor import main
+        from house_pricing_predictor import main
     except ImportError as e:
         pytest.fail(f"Failed to import main module: {str(e)}")
 
     try:
-        from src.house_pricing_predictor import data_ingestion
+        from house_pricing_predictor import data_ingestion
     except ImportError as e:
         pytest.fail(f"Failed to import DataIngestion module: {str(e)}")
 
     try:
-        from src.house_pricing_predictor import model_training
+        from house_pricing_predictor import model_training
     except ImportError as e:
         pytest.fail(f"Failed to import ModelTraining module: {str(e)}")
 
     try:
-        from src.house_pricing_predictor import model_scoring
+        from house_pricing_predictor import model_scoring
     except ImportError as e:
         pytest.fail(f"Failed to import ModelScoring module: {str(e)}")
 
     try:
-        from src.house_pricing_predictor.data_ingestion import load_housing_data
+        from house_pricing_predictor.data_ingestion import load_housing_data
 
         assert callable(load_housing_data)
     except ImportError as e:
@@ -54,7 +47,7 @@ def test_package_installation():
         )
 
     try:
-        from src.house_pricing_predictor.model_training import model_training
+        from house_pricing_predictor.model_training import model_training
 
         assert callable(model_training)
     except ImportError as e:
@@ -63,7 +56,7 @@ def test_package_installation():
         )
 
     try:
-        from src.house_pricing_predictor.model_scoring import model_scoring
+        from house_pricing_predictor.model_scoring import model_scoring
 
         assert callable(model_scoring)
     except ImportError as e:
